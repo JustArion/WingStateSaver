934afac0ca39915706938e7933dc576c08f8abb5
arion <53127594+Arion-Kun@users.noreply.github.com>
2021-11-05 03:46:02 +0200

    Initial Commit

---
+++ src/Start.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Dawn.WingStateSave;
+using MelonLoader;
+
+[assembly: MelonColor(ConsoleColor.DarkCyan)]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonInfo(typeof(Start), "WingMenuStateSaver", "1.0.0", "arion#1223")]
+
+namespace Dawn.WingStateSave
+{
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using UnhollowerRuntimeLib;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using VRC.UI.Elements;
+
+    internal sealed class Start : MelonMod
+    {
+        
+        public override void OnApplicationStart()
+        {
+            var cat = MelonPreferences.CreateCategory("WingStateSaver", "Wing Menu State Saver");
+            LeftEnabledSaveState = cat.CreateEntry("Left Wing", true, "Left Wing", "Changes require a restart.").Value;
+            RightEnabledSaveState = cat.CreateEntry("Right Wing", true, "Right Wing", "Changes require a restart.").Value;
+            ClassInjector.RegisterTypeInIl2Cpp<WingMenuStateSaver>(true);
+        }
+
+        private static Scene DontDestroyOnLoad
+        {
+            get
+            {
+                try
+                {
+                    var Cow = new GameObject("Moo");
+                    UnityEngine.Object.DontDestroyOnLoad(Cow);
+                    var SacrificedCow = Cow.scene;
+                    UnityEngine.Object.DestroyImmediate(Cow);
+                    return SacrificedCow;
+                }
+                catch
+                {
+                    return default;
+                }
+            }
+        }
+        private static bool? _Initialized = false;
+        private static bool AssumedQMCreated => DontDestroyOnLoad != default && DontDestroyOnLoad.rootCount > 9;
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (_Initialized is not false || !AssumedQMCreated) return;
+            _Initialized = null;
+            LateUiManagerInit();
+        }
+        
+        internal static bool LeftEnabledSaveState;
+        internal static bool RightEnabledSaveState;
+        private static QuickMenu _QuickMenu;
+        internal static QuickMenu QuickMenu => _QuickMenu ??= UnityEngine.Resources.FindObjectsOfTypeAll<QuickMenu>().FirstOrDefault(p => p.gameObject.scene.name == "DontDestroyOnLoad");
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void LateUiManagerInit()
+        {
+            if (MelonDebug.IsEnabled())
+                MelonDebug.Msg("LateUiManagerInit");
+            
+            if (QuickMenu is null)
+                MelonLogger.Error("Unable to find <VRC.UI.Elements.QuickMenu>");
+            else
+                QuickMenu.gameObject.AddComponent<WingMenuStateSaver>();
+        }
+    }
+}
---
+++ src/WingMenuStateSaver.cs
@@ -0,0 +1,63 @@
+﻿namespace Dawn.WingStateSave
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+    using MelonLoader;
+    using UnhollowerBaseLib.Attributes;
+    using UnityEngine;
+    using VRC.UI.Elements;
+
+    public class WingMenuStateSaver : MonoBehaviour
+    {
+        public WingMenuStateSaver(IntPtr ptr) : base(ptr) {}
+        
+        private static PropertyInfo WingToggleButton;
+        [HideFromIl2Cpp]
+        private static void Toggle(Wing instance)
+        {
+            if (instance is null) throw new ArgumentNullException(nameof(instance));
+            
+            if (WingToggleButton is { }) ((UnityEngine.UI.Button)WingToggleButton.GetValue(instance)).Press();
+            else
+            {
+                WingToggleButton = typeof(Wing).GetProperties(BindingFlags.Public | BindingFlags.Instance).First(p =>
+                    p.PropertyType == typeof(UnityEngine.UI.Button) &&
+                    ((UnityEngine.UI.Button)p.GetValue(instance)).name == "Button");
+                ((UnityEngine.UI.Button)WingToggleButton.GetValue(instance)).Press();
+            }
+
+        }
+
+        [HideFromIl2Cpp][MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Wing GetWing(Wing.WingPanel panel) =>
+            Start.QuickMenu.prop_MenuStateController_0.field_Private_ArrayOf_Wing_0.First(w =>
+                w.field_Public_WingPanel_0 == panel);
+
+        [HideFromIl2Cpp]
+        private IEnumerator EnableCoroutine()
+        {
+            yield return null;
+            //yield return new WaitForSeconds(0.2f); // Wait for QM OnEnable Initialize
+            if (Start.LeftEnabledSaveState)
+                Toggle(GetWing(Wing.WingPanel.Left));
+
+            if (Start.RightEnabledSaveState)
+                Toggle(GetWing(Wing.WingPanel.Right));
+            
+            //Shhh, don't tell anyone
+            MelonLogger.Msg(Start.LeftEnabledSaveState 
+                ? Start.RightEnabledSaveState 
+                    ? "Showing Left & Right Wings" 
+                    : "Showing Left Wing" 
+                : Start.RightEnabledSaveState 
+                    ? "Showing Right Wing" 
+                    : "Default Wing State");
+            
+            Destroy(this);
+        }
+        private void OnEnable()=> MelonCoroutines.Start(EnableCoroutine());
+    }
+}
